21c0c09d233ed63d7459a9a702ea1c72532625f0
Mayada Abo Omar <mayadaaboomar16@gmail.com>
2026-07-31 12:00:32 +0300

    Solve Problem 6

---
+++ problem 6/src/AtmPinVerification.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class AtmPinVerification {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int correctPin = 1234;
+        System.out.print("Enter PIN: ");
+        int pin = input.nextInt();
+        if (pin == correctPin) {
+            System.out.println("Access Granted");
+        } else {
+            System.out.println("Invalid PIN");
+        }
+    }
+}
